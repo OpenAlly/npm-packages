f2c10b28c265f282a76d37e4bc6645d9c2fac256
Thomas.G <gentilhomme.thomas@gmail.com>
2023-09-30 00:32:42 +0200

    feat: implement auto-url package (#44)

---
+++ src/auto-url/test/autoURL.spec.ts
@@ -0,0 +1,97 @@
+// Import Node.js Dependencies
+import { describe, it, test } from "node:test";
+import assert from "node:assert/strict";
+
+// Import Internal Dependencies
+import autoURL, { autoURLTransformConfig } from "../src/index";
+// eslint-disable-next-line no-duplicate-imports
+import { autoURL as autoURLBis } from "../src/index";
+
+// CONSTANTS
+const kDummyURL = new URL("https://google.fr");
+
+describe("autoURL", () => {
+  test("should be a function", () => {
+    assert.ok(typeof autoURL === "function");
+    assert.ok(typeof autoURLBis === "function");
+  });
+
+  test("should export the same function as default & unit", () => {
+    assert.ok(autoURL === autoURLBis);
+  });
+
+  test("should return no parameters if payload is not defined", () => {
+    const url = autoURL(kDummyURL);
+
+    assert.strictEqual(url.searchParams.size, 0);
+  });
+
+  test("should throw a runtime Error if source is not defined (null or undefined)", () => {
+    assert.throws(
+      () => autoURL(undefined as any),
+      {
+        name: "Error",
+        message: "source argument must be provided"
+      }
+    );
+  });
+
+  describe("non iterable payload", () => {
+    it("should generate a new URL with one argument", () => {
+      const url = autoURL(kDummyURL, {
+        foo: "bar"
+      });
+
+      assert.ok(url !== kDummyURL);
+      assert.strictEqual(url.href, "https://google.fr/?foo=bar");
+
+      assert.strictEqual(url.searchParams.size, 1);
+      assert.strictEqual(url.searchParams.get("foo"), "bar");
+    });
+
+    it("should generate a new URL with multiple arguments", () => {
+      const url = autoURL(kDummyURL, {
+        aaa: 5,
+        bbb: [1, 2, 3]
+      });
+
+      assert.strictEqual(url.searchParams.size, 2);
+      assert.strictEqual(url.searchParams.get("aaa"), "5");
+      assert.strictEqual(url.searchParams.get("bbb"), "[1,2,3]");
+    });
+  });
+
+  describe("iterable payload", () => {
+    it("should generate a new URL with an Array of pair", () => {
+      const url = autoURL(kDummyURL, [
+        ["foo", "bar"]
+      ]);
+
+      assert.strictEqual(url.searchParams.size, 1);
+      assert.strictEqual(url.searchParams.get("foo"), "bar");
+    });
+
+    it("should generate a new URL with a Map", () => {
+      const url = autoURL(
+        kDummyURL,
+        new Map([["foo", "bar"]])
+      );
+
+      assert.strictEqual(url.searchParams.size, 1);
+      assert.strictEqual(url.searchParams.get("foo"), "bar");
+    });
+  });
+
+  describe("transformers", () => {
+    it("should use an UpperCase transformer", () => {
+      const upperCaseTransformerConfig: autoURLTransformConfig<"foo"> = {
+        foo: (value) => value.toUpperCase()
+      };
+
+      const url = autoURL(kDummyURL, {
+        foo: "bar"
+      }, upperCaseTransformerConfig);
+      assert.strictEqual(url.searchParams.get("foo"), "BAR");
+    });
+  });
+});
